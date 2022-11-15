ccc38f4302054cc4cb0166e7017681dc0e903cf3
Trongnguyen29603 <115914589+Trongnguyen29603@users.noreply.github.com>
2022-11-15 19:45:51 +0700

    slick slider

---
+++ layout/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".rooms").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
+        arrows: false,
+        // dots:true,
+    });
+});
